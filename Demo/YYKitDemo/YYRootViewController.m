e705dd4295d1b1de20dc95f54663168b3b0e2b38
maxwellli <maxwellli@tencent.com>
2020-08-23 13:33:45 +0800

    修改名字

--- Demo/YYKitDemo/YYRootViewController.m
+++ Demo/YYKitDemo/YYRootViewController.m
@@ -35,9 +35,9 @@
     self.title = @"YYKit Example";
     self.titles = @[].mutableCopy;
     self.classNames = @[].mutableCopy;
-    [self addCell:@"Model" class:@"YYModelExample"];
-    [self addCell:@"Image" class:@"YYImageExample"];
-    [self addCell:@"Text" class:@"YYTextExample"];
+    [self addCell:@"YYKit" class:@"YYModelExample"];
+    [self addCell:@"LCLRUManager" class:@"YYImageExample"];
+    [self addCell:@"NSCache" class:@"YYTextExample"];
 //    [self addCell:@"Utility" class:@"YYUtilityExample"];
     [self addCell:@"Feed List Demo" class:@"YYFeedListExample"];
     [self.tableView reloadData];
@@ -131,7 +131,7 @@
     NSString *data = @"iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii";
     for (int i = 0; i < kLoopCpunt; i ++) {
         NSString *key = @(i).stringValue;
-        [self.circleLinkListManager setObject:data forKey:key withCost:1024 * 10];
+        [self.circleLinkListManager setObject:data forKey:key cost:1024 * 10];
     }
     NSTimeInterval end = [[NSDate date] timeIntervalSince1970];
     NSLog(@"CircleLinkList cost = %f", (end - start));
@@ -149,7 +149,7 @@
     NSString *data = @"iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii";
     for (int i = 0; i < kLoopCpunt; i ++) {
         NSString *key = @(i).stringValue;
-        [manager setObject:data forKey:key withCost:data.length * 2];
+        [manager setObject:data forKey:key cost:data.length * 2];
     }
     NSTimeInterval end = [[NSDate date] timeIntervalSince1970];
     NSLog(@"CircleLinkList count = %ld cost = %f", (long)count, (end - start));
@@ -173,7 +173,7 @@
     NSUInteger singleCost = count * 1024 * 1024 / kFullSize;
     for (int i = 0; i < kLoopCpunt; i ++) {
         NSString *key = @(i).stringValue;
-        [manager setObject:data forKey:key withCost:singleCost];
+        [manager setObject:data forKey:key cost:singleCost];
     }
     NSTimeInterval end = [[NSDate date] timeIntervalSince1970];
     NSLog(@"CircleLinkList size limnit = %ld MB singleSize = %ld cost = %f", (long)count, singleCost, (end - start));
